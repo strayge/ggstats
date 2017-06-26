5bed5b549afc9b7a83deb0e85df6402efb5af27b
strayge <strayge@gmail.com>
2017-06-27 01:43:15 +0600

    start

---
+++ ggchat/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GgchatConfig(AppConfig):
+    name = 'ggchat'
---
+++ ggchat/management/commands/fetcher.py
@@ -0,0 +1,20 @@
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def add_arguments(self, parser):
+        # parser.add_argument('poll_id', nargs='+', type=int)
+        pass
+
+    def handle(self, *args, **options):
+        # for poll_id in options['poll_id']:
+        #     try:
+        #         poll = Poll.objects.get(pk=poll_id)
+        #     except Poll.DoesNotExist:
+        #         raise CommandError('Poll "%s" does not exist' % poll_id)
+        #
+        #     poll.opened = False
+        #     poll.save()
+        self.stdout.write(self.style.SUCCESS('Successfully'))
