8f7a5d25073a5f195e85eebfa3ce230af61cc4b0
strayge <strayge@gmail.com>
2017-07-07 04:13:10 +0600

    added money page

--- ggchat/views.py
+++ ggchat/views.py
@@ -46,16 +46,48 @@ def chart(request):
 
 
 def money(request):
-    headers = ['Pag1', 'Page2', 'Pag3e', 'Page4']
-    data = [['/index.html', '1265', '32.3%', '$321.33', ],
-            ['/about.html', '261', '33.3%', '$234.12', ],
-            ['/sales.html', '665', '21.3%', '$16.34', ], ]
+    week_ago = timezone.now() - datetime.timedelta(days=7)
+    donates_data = Donation.objects.filter(timestamp__gte=week_ago).order_by('-timestamp').values('user_id', 'channel_id', 'amount', 'timestamp')
 
-    return render_to_response('ggchat/table.html', {'headers': headers,
-                                                    'data': data,
-                                                    'title': 'Деньги',
-                                                    'text_pre': 'pre',
-                                                    'text_after': 'after'})
+    grouped_by_date = {}
+    for donate in donates_data:
+        date = donate['timestamp'].date()
+        grouped_by_date[date] = grouped_by_date.get(date, 0) + donate['amount']
+    grouped_by_date_list = []
+    for date, amount in grouped_by_date.items():
+        grouped_by_date_list.append({'date': date, 'amount': int(amount)})
+
+    chart_donate_total = {'data': grouped_by_date_list,
+                          'x_keyword': 'date',
+                          'y_keyword': 'amount',
+                          'type': 'column',
+                          'name': 'Донат',
+                          'title': '',
+                          'y_title': 'Рублей',
+                          }
+
+    grouped_by_channel = {}
+    for donate in donates_data:
+        channel_id = donate['channel_id']
+        # skip donates to cups
+        if channel_id is None:
+            continue
+        grouped_by_channel[channel_id] = grouped_by_channel.get(channel_id, 0) + donate['amount']
+    grouped_by_channel_list = []
+    for channel_id, amount in grouped_by_channel.items():
+        channel = Channel.objects.filter(channel_id=channel_id).first()
+        if channel and channel.streamer:
+            streamer_name = channel.streamer.username
+        else:
+            streamer_name = ''
+        grouped_by_channel_list.append({'channel_id': channel_id, 'amount': int(amount), 'username': streamer_name})
+    grouped_by_channel_list.sort(key=lambda x: x['amount'], reverse=True)
+
+    content = {'chart_donate_total': chart_donate_total,
+               'donate_top_per_channel': grouped_by_channel_list[:15],
+               }
+
+    return render_to_response('ggchat/money.html', content)
 
 
 def user(request, user_id):
@@ -132,7 +164,7 @@ def channel(request, channel_id):
                     'type': 'column',
                     'name': 'Донат',
 
-                    'zoom': True,
+                    # 'zoom': True,
                     'legend': True,
                     'title': '',
                     'y_title': 'Рублей',
