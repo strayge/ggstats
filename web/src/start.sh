22bdad876bcf16bee3a099381545ec035b62c85c
strayge <strayge@gmail.com>
2018-09-10 02:11:06 +0600

    increased timeout up to 120 secs

--- web/src/start.sh
+++ web/src/start.sh
@@ -8,4 +8,4 @@ fi
 
 # Start Gunicorn processes
 echo Starting Gunicorn...
-exec gunicorn project.wsgi:application --bind 0.0.0.0:8080 --workers 3
+exec gunicorn project.wsgi:application --bind 0.0.0.0:8080 --workers 3 --timeout 120
