5c41514590996790fb344da96e5b92f4adeba5f8
Sergey O <strayge@gmail.com>
2019-08-14 14:19:15 +0300

    ggstats: disabled old ws

--- web/src/ggchat/management/commands/_ws_client.py
+++ web/src/ggchat/management/commands/_ws_client.py
@@ -13,10 +13,10 @@ WS_ENDPOINT = 'wss://chat-1.goodgame.ru/chat2/'
 PREDEFINED_CHANNELS = [] #'5', '94546', 'r128', '13214']
 
 class ChatWsClient(WsBaseClient):
-    def __init__(self, log_level, queue_msg_parse, queue_old_cmd, queue_old_cmd_resp):
+    def __init__(self, log_level, queue_msg_parse, queue_old_cmd=None, queue_old_cmd_resp=None):
         self.queue_msg_parse = queue_msg_parse
-        self.queue_old_cmd = queue_old_cmd
-        self.queue_old_cmd_resp = queue_old_cmd_resp
+        # self.queue_old_cmd = queue_old_cmd
+        # self.queue_old_cmd_resp = queue_old_cmd_resp
 
         self.joined_channels = set()
         self.last_channels_list_check = 0
@@ -33,9 +33,9 @@ class ChatWsClient(WsBaseClient):
             await self.join_channel(channel_id)
 
     async def ws_periodic_tasks(self):
-        while not self.queue_old_cmd_resp.empty():
-            received = self.queue_old_cmd_resp.get()
-            await self.ws_received(received)
+        # while not self.queue_old_cmd_resp.empty():
+        #     received = self.queue_old_cmd_resp.get()
+        #     await self.ws_received(received)
         if time.time() > self.last_users_check + self.CHECK_USERS_PERIOD:
             self.log.info('users check')
             self.last_users_check = time.time()
@@ -49,19 +49,19 @@ class ChatWsClient(WsBaseClient):
             await self.request_channels()
 
     async def join_channel(self, channel_id):
-        self.queue_old_cmd.put({'type': 'join', 'data': channel_id})
+        # self.queue_old_cmd.put({'type': 'join', 'data': channel_id})
         join_channel_query = {"type": "join", "data": {"channel_id": str(channel_id), "hidden": False}}
         await self.send(join_channel_query)
 
     async def request_channels(self):
         # sync channels
-        self.queue_old_cmd.put({'type': 'channels', 'data': self.joined_channels})
+        # self.queue_old_cmd.put({'type': 'channels', 'data': self.joined_channels})
 
         get_channels_query = {"type": "get_channels_list", "data": {"start": 0, "count": 200}}
         await self.send(get_channels_query)
         # fallback
-        for i in range(0,6):
-            self.queue_old_cmd.put({'type': 'send', 'data': {"type": "get_channels_list", "data": {"start": i*45, "count": 50}}})
+        # for i in range(0,6):
+        #     self.queue_old_cmd.put({'type': 'send', 'data': {"type": "get_channels_list", "data": {"start": i*45, "count": 50}}})
 
     async def send(self, data):
         s = json.dumps(data)
@@ -76,7 +76,7 @@ class ChatWsClient(WsBaseClient):
         if msg_type == 'welcome':
             pass
         elif msg_type == 'channels_list':
-            self.log.info('channels_list answer ({} streams)'.format(len(msg['data']['channels'])))
+            self.log.debug('channels_list answer ({} streams)'.format(len(msg['data']['channels'])))
             try:
                 for channel in msg['data']['channels']:
                     channel_id = channel['channel_id']
--- web/src/ggchat/management/commands/fetcher2.py
+++ web/src/ggchat/management/commands/fetcher2.py
@@ -8,7 +8,7 @@ from django.core.management import BaseCommand
 from ggchat.management.commands._ws_client import ChatWsClient
 from ggchat.management.commands._msg_parser import ChatMsgParser
 from ggchat.management.commands._common import setup_logger
-from ggchat.management.commands._ws_client_old import ChatWsClientOld
+# from ggchat.management.commands._ws_client_old import ChatWsClientOld
 
 
 class Command(BaseCommand):
@@ -18,21 +18,21 @@ class Command(BaseCommand):
         log_level = logging.INFO
         log = setup_logger(log_level)
         queue_msg_parse = Queue()
-        queue_old_cmd = Queue()
-        queue_old_cmd_resp = Queue()
+        # queue_old_cmd = Queue()
+        # queue_old_cmd_resp = Queue()
         try:
             kwargs = {'log_level': log_level,
                       'queue_msg_parse': queue_msg_parse,
-                      'queue_old_cmd': queue_old_cmd,
-                      'queue_old_cmd_resp': queue_old_cmd_resp,
+                      # 'queue_old_cmd': queue_old_cmd,
+                      # 'queue_old_cmd_resp': queue_old_cmd_resp,
                       }
             Process(target=ChatWsClient, kwargs=kwargs, name='ws').start()
 
-            kwargs = {'log_level': log_level,
-                      'queue_old_cmd': queue_old_cmd,
-                      'queue_old_cmd_resp': queue_old_cmd_resp,
-                      }
-            Process(target=ChatWsClientOld, kwargs=kwargs, name='ws_old').start()
+            # kwargs = {'log_level': log_level,
+            #           'queue_old_cmd': queue_old_cmd,
+            #           'queue_old_cmd_resp': queue_old_cmd_resp,
+            #           }
+            # Process(target=ChatWsClientOld, kwargs=kwargs, name='ws_old').start()
 
             kwargs = {'log_level': log_level,
                       'queue_msg_parse': queue_msg_parse,
